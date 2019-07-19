c9f2d07c7ea2e963fbcce7eb4ced580efae2918f
Ubuntu <ubuntu@ip-172-31-32-224.ec2.internal>
2019-07-19 18:26:16 +0000

    finally we have a graph!

--- js/script.js
+++ js/script.js
@@ -2,26 +2,41 @@ queue()
     .defer(d3.json, "../data/transactionData.json")
     .await(makeGraphs);
 
+  // fetch("../data/transactionData.json")
+   // .then(function(results){
+     //   results.json().then(function(results){
+       //  makeGraphs(null,results);
+//        });
+  //  });
+    
 function makeGraphs(error, transactionData) {
+    console.log(transactionData);
     var ndx = crossfilter(transactionData);
     var store_dim = ndx.dimension(dc.pluck('store'));
     var total_score_per_store = store_dim.group().reduceSum(dc.pluck('cost'));
 
-    show_test(ndx)
+   show_test(ndx);
 
     dc.renderAll();
 
+console.log(d3);
 
-dc.barChart('#Test')
-    .width(300)
-    .height(500)
+function show_test(ndx){
+    var VariableDim= ndx.dimension(dc.pluck('store'))
+
+
+dc.barChart("#test")
+    .width(600)
+    .height(300)
     .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-    .diimension(store_dim)
+    .dimension(store_dim)
     .group(total_score_per_store)
     .transitionDuration(1000)
     .x(d3.scale.ordinal())
-    .xUnits(dc.units.ordinal)
+    .xUnits(dc.units.ordinal) 
     .xAxisLabel("The Stores!")
-    .yAxis().ticks(1);
+    .yAxis().ticks(6);
+    
 
+}
 }
